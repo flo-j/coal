b99d00cfefcf816d28ccf750cf89ff546ef14dd4
flo_j <florence@jornod.com>
2016-03-31 14:59:39 +0200

    rajout node

--- node_test.cpp
+++ node_test.cpp
@@ -1,22 +1,76 @@
 /* 	florence jornod
 	aucune garantie
 */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
 class Node{
+public:
 	Node* pParent; // a terme une paire? vu que deux parents.. mais louche.. 
-	vector <Node&> m_children; // vecteur de reference sur les enfants : evite la copie.. 
-	String m_status; // pour les parcours!
+	vector<Node*> m_children; // vecteur de reference sur les enfants : evite la copie.. 
+	string m_status; // pour les parcours!
 	int m_data; // attention à terme ça devrait etre autre chose qu'un int.. voir comment utiliser les using !
 
-	Node(pParent=nullptr,m_status="non atteint"){} // constructeur par defaut 
-	Node(pParent=nullptr,m_status="non atteint",int data){ // si on y ajoute des données
+
+public:
+	Node(Node* pParent=nullptr, string m_status="non atteint"){} // constructeur par defaut 
+	Node(int data){ // si on y ajoute des données
+		pParent=nullptr;
+		m_status="non atteint";
 		m_data=data;
 	} 
 
+	void change_status(string status){
+		m_status=status;
+	}
 
-void mergeWith(Node node1){
+	void AddAChild(Node &node1){
+		m_children.push_back(&node1);
+	}
 
-}
+	void hasForChildren(Node &node1, Node &node2){
+		//
+		node1.pParent=this; // normalement this correspond au pointeur qui pointe sur le pere ..
+		node2.pParent=this;
+		AddAChild(node1);
+		AddAChild(node2);
+	}
 
+	void Dfs(){
+		Node*=r;
+		Node*=voisin;
+		stack<Node*> st;
+		st.push(this);
+		this->m_status="atteint";
+		while(!st.empty()){
+			r=st.top();
+			st.pop();
+			
+		}
+	}
+
+	void print_node(){
+		cout << " le noeud :" ;
+		cout << m_data ;
+		cout << " et l'adresse est :";
+		cout << this;
+		if(pParent!=0){
+			cout <<" a pour parent :";
+			cout << pParent << " de valeur : " ;
+			cout << pParent->m_data ;
+		}else{	
+			cout << " n'a pas de parents";
+		}
+		cout << " et pour enfants " ;
+		for(unsigned int i=0; i<m_children.size();i++){
+
+			cout << m_children[i]->m_data << " ";
+		}
+		cout << endl;
+	}
 };
 
 int main(){
@@ -28,13 +82,26 @@ int main(){
 	Node node3(2);
 	Node node4(4);
 	
-	node2=node0.mergeWith(node1);
-	node4=node2.mergeWith(node3);
+	//node2=node0.mergeWith(node1);
+	//node4=node2.mergeWith(node3);
+
+	node2.hasForChildren(node0,node1);
+	node4.hasForChildren(node2,node3);
+	node0.print_node();
+	cout <<" adresse node0 :" << &node0 << endl;
+	node1.print_node();
+	node2.print_node();
+	node3.print_node();
+	node4.print_node();
+	cout << node0.m_status << endl;
+	node0.Dfs();
+	cout << node0.m_status << endl;
+
 	//construit un arbre avec node0 et node1 qui ont pour pere node2
 	// node2 et node3 qui ont pour pere node4
 
 	
-	node4.isRoot();
+	//node4.isRoot();
 	// parcours de l'arbre 
-	node4.Dfs();
+	//node4.Dfs();
 }
